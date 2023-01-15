ab20222ce12d4bd30bfeaac68064147a456316ab
Leonardo Caballero <leonardocaballero25@gmail.com>
2023-01-15 01:54:43 -0600

    first commit

---
+++ Controllers/Permisos.php
@@ -0,0 +1,34 @@
+<?php 
+
+	class Permisos extends Controllers{
+		public function __construct()
+		{
+			parent::__construct();
+		}
+
+		public function getPermisosRol(int $idrol)
+		{
+            $rolid = intval($idrol);
+
+            if ($rolid > 0) {
+
+                $arrModulos = $this->model->selectModulos();
+
+				$arrPermisosRol = $this->model->selectPermisosRol($rolid);
+
+				$arrPermisos = array('r' => 0, 'w' => 0, 'u' => 0, 'd' => 0);
+
+				$arrPermisosRol = array('idrol' => $rolid);
+
+				if (empty($arrPermisosRol)) {
+					for ($i=0; $i < count($arrModulos) ; $i++) {
+						
+						$arrModulos[$i]['permisos'] = $arrPermisos;
+					}
+				}
+				dep($arrModulos);
+            }
+		}
+
+	}
+ ?>
